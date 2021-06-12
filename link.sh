25a8a6a18d605a693545ee0e24b221f26564e99e
Mai Thanh Minh <thanhminh.mr@gmail.com>
2021-06-12 21:55:32 +0700

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 build_lower=/tmp/$(uuidgen)
 build_upper=/tmp/$(uuidgen)
 build_workdir=/tmp/$(uuidgen)
 mkdir ${build_lower} ${build_upper} ${build_workdir}
 sudo mount -t overlay -o lowerdir=${build_lower},upperdir=${build_upper},workdir=${build_workdir} none ./build
-
-sudo mount -t overlay -o lowerdir=./personal/lower,upperdir=./personal/upper,workdir=./personal/workdir none ./repository
